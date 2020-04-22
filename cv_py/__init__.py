7badee323c7cea00313b1571465ffc8959f3d5fe
tbsexton <tbsexton@asu.edu>
2020-04-22 14:40:02 -0400

    auto versions placeholder

--- cv_py/__init__.py
+++ cv_py/__init__.py
@@ -1,6 +1,6 @@
 __title__ = "cv-py"
 # __version__ = get_version() # TODO
-__version__ = "0.0.0"
+__version__ = "0.0.0"  # placeholder for dynamic version
 __compatible__ = "0.1.1"
 __release__ = False
 __download_url__ = "https://github.com/usnistgov/cord19-cdcs-nist/releases"
